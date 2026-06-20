861b672287053940465100e941b4e264c322719c
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-06-20 19:08:05 +0600

    post method implemented.

--- src/database/database.ts
+++ src/database/database.ts
@@ -8,13 +8,13 @@ const initDB = async () => {
         name VARCHAR(20) NOT NULL,
         email VARCHAR(50) UNIQUE NOT NULL,
         password VARCHAR(20) NOT NULL,
-        role VARCHAR(20),
+        role VARCHAR(20) DEFAULT 'contributor' CHECK (role IN ('contributor', 'maintainer')),
 
         created_at TIMESTAMP DEFAULT NOW(),
         updated_at TIMESTAMP DEFAULT NOW()
         )
         `)
-        console.log("✅ Users table created or verified successfully!");
+        console.log("Database Created Successfully");
     } catch (error: any) {
         console.error("❌ Database initialization failed:");
         console.error(error.message);
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
-import express, { type Application } from "express"
+import express, { type Application, type Request, type Response } from "express"
 import config from "./config/config";
 import initDB from "./database/database";
+import pool from "./app";
 
 const app: Application = express()
 const port = config.port
@@ -20,14 +21,34 @@ app.get('/', (req, res) => {
     });
 });
 
-app.post('/', async (req, res) => {
-    const body = req.body;
-    const { name, email, address } = body
-    res.status(201).json({
-        success: true,
-        message: "User Created Successfully",
-        data: { name, email, address }
-    })
+app.post('/', async (req: Request, res: Response) => {
+    const body = req.body
+    const { name, email, password, role = 'contributor' } = body
+    try {
+        const result = await pool.query(`
+         INSERT INTO users(name,email,password,role) VALUES ($1,$2,$3,$4)   
+         RETURNING  id,name,email,role,created_at,updated_at`, [name, email, password, role])
+
+        res.status(201).json({
+            success: true,
+            message: "User registered successfully",
+            data: result.rows[0]
+        });
+    } catch (error: any) {
+        if (error.code === '23505') {
+            res.status(400).json({
+                success: false,
+                message: "User Already Exists",
+                errors: "Email is already registered"
+            });
+            return;
+        }
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+            errors: error.message
+        });
+    }
 })
 
 app.listen(port, () => {
