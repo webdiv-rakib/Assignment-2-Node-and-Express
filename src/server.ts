4f38f204d0dacf9c42dfbd9618d56efa1329e477
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-06-20 17:17:07 +0600

    working on req and res.

--- src/server.ts
+++ src/server.ts
@@ -2,32 +2,25 @@ import express, { type Application } from "express"
 const app: Application = express()
 const port = 5000
 
-app.use(express.json());
-app.use(express.text());
-app.use(express.urlencoded({ extended: true }))
-
+app.use(express.json())
 app.get('/', (req, res) => {
     res.status(200).json({
         success: true,
+        sever: "Assignment Server Running",
         message: "Assignment-2",
         author: "webdiv-rakib"
     });
 });
 
 app.post('/', async (req, res) => {
-    const body = req.body
-    const { name, email, password, address } = body;
+    const body = req.body;
+    const { name, email, address } = body
     res.status(201).json({
         success: true,
         message: "User Created Successfully",
-        data: {
-            name,
-            email,
-            address
-        }
-    });
-});
-// working on server.ts
+        data: { name, email, address }
+    })
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
