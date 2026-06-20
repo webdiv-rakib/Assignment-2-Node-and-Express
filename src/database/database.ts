744ffce9dd28c2f9f2e6359690fa8e854a353c99
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-06-21 01:56:23 +0600

    issues table created in database.

--- src/database/database.ts
+++ src/database/database.ts
@@ -19,7 +19,21 @@ const initDB = async () => {
         created_at TIMESTAMP DEFAULT NOW(),
         updated_at TIMESTAMP DEFAULT NOW()
         )
-        `)
+        `);
+
+        await pool.query(`
+        CREATE TABLE IF NOT EXISTS issues(
+        id SERIAL PRIMARY KEY,
+        reporter_id INT NOT NULL,
+        title VARCHAR(150) NOT NULL,
+        description TEXT NOT NULL,
+        type VARCHAR(20) NOT NULL CHECK (type IN ('bug', 'feature_request')) ,
+        status VARCHAR(20) DEFAULT 'open' CHECK(status IN('open','in_progress','resolved')),
+
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )   
+            `)
         console.log("Database Created Successfully");
     } catch (error: any) {
         console.error("❌ Database initialization failed:");
