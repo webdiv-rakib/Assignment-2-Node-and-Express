6ae1fd063a56666fbdd7bc327c3d706dd57d2055
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-06-22 02:34:06 +0600

    logger implemented to save record.

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import initDB, { pool } from "./database/database";
 import { userRoute } from "./modules/user/user.route";
 import { issueRoute } from "./modules/issue/issue.router";
 import { authRoute } from "./modules/auth/auth.route";
+import logger from "./middleware/logger";
 
 const app: Application = express();
 // middleware
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
+app.use(logger);
 
 app.use('/api/users', userRoute);
 app.use('/api/issues', issueRoute);
---
+++ src/middleware/logger.ts
@@ -0,0 +1,14 @@
+import type { NextFunction, Request, Response } from "express";
+import fs from "fs";
+const logger = (req: Request, res: Response, next: NextFunction) => {
+    const currentTime = new Date().toLocaleString();
+    const log = `[${currentTime}] Method: ${req.method} | URL: ${req.url}\n`;
+
+    fs.appendFile('logger.txt', log, (error) => {
+        if (error) {
+            console.error("❌ Failed to write to logger.txt:", error.message);
+        }
+    });
+    next();
+};
+export default logger;
