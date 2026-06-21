7e903256c9542359d8eb205d71592fb121bff03f
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-06-21 23:39:06 +0600

    database sql updated as required.

--- src/database/database.ts
+++ src/database/database.ts
@@ -10,28 +10,28 @@ const initDB = async () => {
     try {
         await pool.query(`
         CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(20) NOT NULL,
-        email VARCHAR(50) UNIQUE NOT NULL,
-        password TEXT NOT NULL,
-        role VARCHAR(20) DEFAULT 'contributor' CHECK (role IN ('contributor', 'maintainer')),
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            role VARCHAR(20) DEFAULT 'contributor' CHECK (role IN ('contributor', 'maintainer')),
 
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
         )
         `);
 
         await pool.query(`
         CREATE TABLE IF NOT EXISTS issues(
-        id SERIAL PRIMARY KEY,
-        reporter_id INT NOT NULL,
-        title VARCHAR(150) NOT NULL,
-        description TEXT NOT NULL,
-        type VARCHAR(20) NOT NULL CHECK (type IN ('bug', 'feature_request')) ,
-        status VARCHAR(20) DEFAULT 'open' CHECK(status IN('open','in_progress','resolved')),
+            id SERIAL PRIMARY KEY,
+            reporter_id INT NOT NULL,
+            title VARCHAR(150) NOT NULL,
+            description TEXT NOT NULL CHECK (LENGTH(TRIM(description)) >= 20),
+            type VARCHAR(20) NOT NULL CHECK (type IN ('bug', 'feature_request')) ,
+            status VARCHAR(20) DEFAULT 'open' CHECK(status IN('open','in_progress','resolved')),
 
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
         )   
             `)
         console.log("Database Created Successfully");
