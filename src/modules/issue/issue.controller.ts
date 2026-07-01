90a95206ddbc4bcbee8e76c29b4677794a3860ff
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-07-01 20:08:45 +0600

    worked on issue controller dry.

--- src/modules/issue/issue.controller.ts
+++ src/modules/issue/issue.controller.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 import { issueService } from "./issue.service";
 import type { AuthUser, UpdateIssuePayload } from "./issue.interface";
+import { sendResponse } from "../../utility/sendResponse";
 
 const createIssue = async (req: Request, res: Response): Promise<void> => {
     // const body = req.body;
@@ -10,19 +11,27 @@ const createIssue = async (req: Request, res: Response): Promise<void> => {
     try {
         const result = await issueService.createIssueIntoDB(reporter_id, req.body);
         if (!reporter_id) {
-            res.status(401).json({ success: false, message: "Unauthorized", errors: "User ID missing" });
+            sendResponse(res, {
+                success: false,
+                statusCode: 401,
+                message: "Unauthorized",
+                error: "User ID missing"
+            });
+            // res.status(401).json({ success: false, message: "Unauthorized", errors: "User ID missing" });
             return;
         }
-        res.status(201).json({
+        sendResponse(res, {
             success: true,
-            message: "Issue created successfully",
+            statusCode: 201,
+            message: "Issue Created Successfully",
             data: result
         });
     } catch (error: any) {
-        res.status(400).json({
+        sendResponse(res, {
             success: false,
+            statusCode: 400,
             message: "Failed to create issue",
-            errors: error.message
+            error: error.message
         });
     };
 };
@@ -30,16 +39,18 @@ const createIssue = async (req: Request, res: Response): Promise<void> => {
 const getAllIssue = async (req: Request, res: Response): Promise<void> => {
     try {
         const result = await issueService.getAllIssueFromDB(req.query);
-        res.status(200).json({
+        sendResponse(res, {
             success: true,
-            message: "Issues retrieved successfully",
+            statusCode: 200,
+            message: "Issues Retrieved Successfully",
             data: result
         });
     } catch (error: any) {
-        res.status(500).json({
+        sendResponse(res, {
             success: false,
+            statusCode: 500,
             message: "Internal Server Error",
-            errors: error.message
+            error: error.message
         });
     };
 };
@@ -49,24 +60,26 @@ const getSingleIssue = async (req: Request, res: Response): Promise<void> => {
     try {
         const result = await issueService.getSingleIssueFromDB(id as string);
         if (!result) {
-            res.status(404).json({
+            sendResponse(res, {
                 success: false,
+                statusCode: 404,
                 message: "Issue not found",
-                errors: `No issue exists with id ${id}`
+                error: `No issue exists with ID: ${id}`
             });
             return;
-        }
-        res.status(200).json({
+        };
+        sendResponse(res, {
             success: true,
+            statusCode: 200,
             message: "Issue retrieved successfully",
-            // FIX 2: Just send 'result' directly instead of 'result.rows[0]'
             data: result
         });
     } catch (error: any) {
-        res.status(500).json({
+        sendResponse(res, {
             success: false,
+            statusCode: 500,
             message: "Internal Server Error",
-            errors: error.message
+            error: error.message
         });
     };
 };
@@ -78,36 +91,36 @@ const updateIssue = async (req: Request, res: Response): Promise<void> => {
     const payload = req.body as UpdateIssuePayload
     try {
         const result = await issueService.updateIssueIntoDB(id as string, user, payload);
-        res.status(200).json({
+        sendResponse(res, {
             success: true,
+            statusCode: 200,
             message: "Issue updated successfully",
             data: result
         });
     } catch (error: any) {
         if (error.message === 'Forbidden Ownership') {
-            res.status(403).json({
+            sendResponse(res, {
                 success: false,
+                statusCode: 403,
                 message: "Forbidden",
-                errors: "You are only allowed to update issues that you created."
+                error: "You are only allowed to update issues that you created."
             });
             return;
         }
-
-        // Handle if the ID doesn't exist at all
         if (error.message === 'Not Found') {
-            res.status(404).json({
+            sendResponse(res, {
                 success: false,
+                statusCode: 404,
                 message: "Not Found",
-                errors: `No issue exists with id ${id}`
+                error: `No issue exists with ID: ${id}`
             });
             return;
-        }
-
-        // Standard server crash fallback
-        res.status(500).json({
+        };
+        sendResponse(res, {
             success: false,
+            statusCode: 500,
             message: "Internal Server Error",
-            errors: error.message
+            error: error.message
         });
     };
 };
@@ -117,22 +130,25 @@ const deleteIssue = async (req: Request, res: Response) => {
     try {
         const result = await issueService.deleteIssueFromDB(id as string);
         if (result.rowCount === 0) {
-            res.status(404).json({
+            sendResponse(res, {
                 success: false,
+                statusCode: 404,
                 message: "Issue not found",
-                errors: `No issue exists with id ${id}`
+                error: `No issue exists with ID: ${id}`
             });
             return;
         };
-        res.status(200).json({
+        sendResponse(res, {
             success: true,
-            message: "Issue deleted successfully"
+            statusCode: 200,
+            message: "Issue Deleted Successfully"
         });
     } catch (error: any) {
-        res.status(500).json({
+        sendResponse(res, {
             success: false,
+            statusCode: 500,
             message: "Internal Server Error",
-            errors: error.message
+            error: error.message
         });
     };
 };
